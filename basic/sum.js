078c2e18c0dc0d7a98171a26225952a3b7c1a028
chiaaaa96 <chiaaaa96@gmail.com>
2021-08-04 21:32:18 +0800

    add the better version

--- basic/sum.js
+++ basic/sum.js
@@ -1,11 +1,15 @@
-console.log("Hello World");
+// console.log("Hello World");
 
 function sum(n) {
-  let sum = 0;
-  for (let i = 1; i <= n; i++) {
-    sum += i;
-  }
-  return sum;
+  return ((n + 1) * n) / 2;
 }
+
+// function sum(n) {
+//   let sum = 0;
+//   for (let i = 1; i <= n; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
 
 console.log(sum(10));
