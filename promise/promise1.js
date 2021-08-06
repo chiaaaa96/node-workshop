8c322a60c20f311bcd708369d28b62cbaa94c192
chiaaaa96 <chiaaaa96@gmail.com>
2021-08-06 14:18:58 +0800

    add promise1.js

---
+++ promise/promise1.js
@@ -0,0 +1,23 @@
+let doWork = fuction (job, timer, isOK) {
+  return new promise((resolve, reject) => {
+    console.log("in promise");
+    setTimeout(() => {
+      let dt = new Date();
+      if (isOK) {
+        resolve(`完成工作: $job at ${dt.toISOString()}`);
+      } else {
+        reject(`失敗了 ${job}`);
+      }
+    }, timer);
+  });
+});
+
+let job = doWork("刷牙", 3000, true);
+job.then{
+  function(resolve) {
+    console.log("第一個函式被呼叫".resolve);
+  },
+  function(reject) {
+    console.log("第二個函式被呼叫".resolve);
+  }
+);
