06c2ce92245dc08774c7172cd7c0808aa8b457bf
chiaaaa96 <chiaaaa96@gmail.com>
2021-08-15 10:43:38 +0800

    add srver.js

---
+++ simple-express/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+// 利用 express 建立了一個 express application
+let app = express();
+
+// HTTP Method: get, post, put, patch, delete
+app.get("/", function (request, response, next) {
+  response.send("Hello");
+});
+
+app.get("/about", function (requset, response, next) {
+  response.send("About");
+});
+
+app.listen(3000, function () {
+  console.log("我們的 web server 啟動了～");
+});
