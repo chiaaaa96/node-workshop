04765f1e40a32f6869907e680acfbdf86bbbeb9e
chiaaaa96 <chiaaaa96@gmail.com>
2021-08-15 11:34:59 +0800

    add Middleware

--- simple-express/server.js
+++ simple-express/server.js
@@ -3,7 +3,21 @@ const express = require("express");
 // 利用 express 建立了一個 express application
 let app = express();
 
+//app.use使用一個「中間件」
+//handler
+app.use((request, response, next) => {
+  let current = new Date();
+  console.log(`有人來囉！ at ${current.toISOString()}`);
+  next();
+});
+
+app.use((request, response, next) => {
+  console.log("second middle ware");
+  next();
+});
+
 // HTTP Method: get, post, put, patch, delete
+//router路由
 app.get("/", function (request, response, next) {
   response.send("Hello");
 });
